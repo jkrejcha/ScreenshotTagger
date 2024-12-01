fc7d37da368be0f881a6e4371c129bb4d4df38c6
Justine Krejcha <justine@justinekrejcha.com>
2024-12-01 02:27:53 -0800

    Version update plus try-catch on the assembly load

--- ScreenshotTagger/Program.cs
+++ ScreenshotTagger/Program.cs
@@ -25,14 +25,22 @@ namespace ScreenshotTagger
 			List<AutotagPlugin> plugins = [];
 			foreach (String file in Directory.GetFiles(PluginsDir, "*.dll", SearchOption.TopDirectoryOnly))
 			{
-				Assembly pluginAssembly = Assembly.LoadFile(file);
-				foreach (Type t in pluginAssembly.GetTypes().Where(t => t.IsSubclassOf(typeof(AutotagPlugin))))
+				Assembly pluginAssembly = Assembly.LoadFrom(Path.GetFullPath(file));
+				try
 				{
-					// TODO: Error checking
-					// TODO: Maybe C compat?
-					plugins.Add((AutotagPlugin)t.GetConstructor([])!.Invoke([]));
+					foreach (Type t in pluginAssembly.GetTypes().Where(t => t.IsSubclassOf(typeof(AutotagPlugin))))
+					{
+						// TODO: Error checking
+						// TODO: Maybe C compat?
+						plugins.Add((AutotagPlugin)t.GetConstructor([])!.Invoke([]));
+					}
+				}
+				catch
+				{
+					// TODO: Handle this
 				}
 			}
+
 
 			// Run the app
 			Application.Run(new FormMain(plugins, args[0])); // TODO: Error checking
